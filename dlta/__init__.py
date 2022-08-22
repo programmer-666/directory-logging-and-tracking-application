f28cbdbaed9dd9cc42901aa1f7d55fab0a4f7669
programmer-666 <suharslan@protonmail.ch>
2022-08-23 00:07:42 +0300

    dir file stuff

--- dlta/__init__.py
+++ dlta/__init__.py
@@ -1,9 +1,13 @@
-import os
-import configparser
+import os, configparser
 
 conf = configparser.ConfigParser()
 conf.read(".conf")
 
 dirs = [x[0] for x in os.walk(conf['DEFAULT']['directory'])]
+files = [os.listdir(x) for x in dirs]
 
-print(dirs)
+print((dirs[0]+"\\"+files[0][0]))
+print(os.path.getsize(dirs[0]+"\\"+files[0][0])) # byte
+print(os.path.getatime(dirs[0]+"\\"+files[0][0])) # access
+print(os.path.getctime(dirs[0]+"\\"+files[0][0])) # metadata change
+print(os.path.getmtime(dirs[0]+"\\"+files[0][0])) # modification
